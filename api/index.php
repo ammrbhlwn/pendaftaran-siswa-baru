c5077918c0bb45d356a17dc6d6c8d321594f088d
ammrbhlwn <150604655+ammrbhlwn@users.noreply.github.com>
2024-11-13 15:16:48 +0700

    update folder

--- api/index.php
+++ api/index.php
@@ -8,7 +8,7 @@ header('Content-Type: text/html; charset=utf-8');
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="styles/style.css" />
+  <link rel="stylesheet" href="../styles/style.css" />
   <title>Pendaftaran Siswa Baru</title>
 </head>
 
